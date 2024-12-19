899b98f948d6b06c1edcfa549b22b205c214a26d
athashri keny <athashrikeny38@gmail.com>
2024-12-19 13:35:42 +0530

    some updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser , refreshTokenAccessToken, changerCurrentUserPassword, getcurrentUser } from "../controllers/user.controllers.js";  // export higger order function from user controllers
+import { loginUser, logoutUser, registerUser , refreshTokenAccessToken, changerCurrentUserPassword, getcurrentUser, GetWatchHistory } from "../controllers/user.controllers.js";  // export higger order function from user controllers
 import { upload } from "../middlewares/multer.js"; // for storaging files eg pdf , photos , avatar imgs
 import { verifyjwt } from "../middlewares/auth.middleware.js";
 
@@ -23,12 +23,21 @@ router.route("/register").post(
 
    // securred routes
    router.route("/logout").post( verifyjwt, logoutUser)
+
    router.route("refresh-token").post(refreshTokenAccessToken)
+
    router.route("/change-password").post(verifyjwt , changerCurrentUserPassword)
+
    router.route("/current-user").get(verifyjwt , getcurrentUser)
+
    router.route("/update-account").patch(verifyjwt)
+
    router.route("/avatar").patch(verifyjwt , upload.single("avatar") , updateUserAvatar)
-   router.route("/coverImage").patch(updateUserCoverImg)
+
+   router.route("/cover-image").patch(single("/cover-Image") , updateUserCoverImg)
+
    router.route("/c/:username").get(verifyjwt , getUserChannelProfile)
+
+   router.route("/history").get(verifyjwt , GetWatchHistory)
 
 export default router;
